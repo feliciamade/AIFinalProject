ec5c7d87eaa53645417e944429bec14ed00e986a
Felicia Phillips  🌱 <codebyberry@gmail.com>
2025-07-31 09:36:25 -0500

    Testing to see if commits show up on primary github repo
    test from Jill

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from "./Footer.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+/*import { faFacebookF,faInstagram, faTwitter} from '@fortawesome/free-brands-svg-icons'; */
+
+function Footer() {
+    return (
+        <footer>
+            <div className={styles.Footer}>
+                    <h2>Root & Revive</h2>
+                <div className={styles.Navigation}>
+                    <a href="/">Home</a>
+                    <a href="/restaurants">Restaurants</a>
+                    <a href="/Contact">Contact</a>
+                    <a href="/about">About Us</a>
+                </div>
+                <div className={styles.Contact}>
+                    <p>Email Address</p>
+                    <p>Phone</p>
+                    <p>Physical Address</p>
+                    <p>Contact Page Link</p>
+                </div>
+                <div className={styles.Legal}>
+                    <a href="/">Privacy</a>
+                    <a href="#">Terms of Service/Use</a>
+                    <a href="#">Cookie Policy</a>
+                    <a href="#">Copyright Notice</a>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
