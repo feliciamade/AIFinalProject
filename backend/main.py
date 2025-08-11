7fefad57f779d1f446100ad7951a82199c530448
asmithxu <93105197+asmithxu@users.noreply.github.com>
2025-08-11 17:28:35 -0500

    Update port

--- backend/main.py
+++ backend/main.py
@@ -105,4 +105,5 @@ async def create_message(message: Message):
     
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
