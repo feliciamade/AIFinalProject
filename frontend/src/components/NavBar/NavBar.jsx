306e370dfd70f31a91364b70958ebf48b65af1e6
Felicia Phillips  🌱 <feliciamp@proton.me>
2025-08-13 11:37:37 -0500

    Update

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useState } from "react";
 import styles from "./NavBar.module.css";
-import Button from "../Button/Button.jsx"; 
+import Button from "../Button/Button.jsx";
+import Popup from "../Popup/Popup.jsx";
 
 function NavBar() {
+  const [isChatOpen, setIsChatOpen] = useState(false);
+
+  const handleChatButtonClick = () => {
+    setIsChatOpen(true);
+  };
+
+  const handleClosePopup = () => {
+    setIsChatOpen(false);
+  };
+
   return (
-    <nav className={styles.navBar} >
+    <nav className={styles.navBar}>
       <h3 className={styles.logo}>Root & Revive</h3>
       <div className={styles.pages}>
-      <a href="/" className={styles.active}>Home</a>
-      <a href="/restaurant">Restaurant</a>
-      <a href="/Contact">Contact</a>
-      <a href="/about">Mission</a>
-      </div>    
-      <Button name ="Ask Herb" />
+        <a href="/" className={styles.active}>Home</a>
+        <a href="/restaurant">Restaurant</a>
+        <a href="/Contact">Contact</a>
+        <a href="/about">Mission</a>
+      </div>
+      <Button name="Ask Herb" onClick={handleChatButtonClick} />
+        
+      {isChatOpen && <Popup onClose={handleClosePopup} />}
     </nav>
   );
 }
 
-export default NavBar; 
+export default NavBar;
